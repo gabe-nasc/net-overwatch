e357db807bb11e3fd605f5abad5b4b9b14b3d984
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2020-03-23 19:47:52 -0300

    Add function to send session data to DB

--- overwatch.js
+++ overwatch.js
@@ -1,6 +1,43 @@
 var ping = require("jjg-ping");
+var axios = require("axios");
 
 const hosts = ["1.1.1.1", "192.168.20.1", "google.com:81", "globo.com"];
+// const hosts = ["1.1.1.1"];
+
+const sessionStartTime = new Date();
+const sessionId = {};
+
+var isNewSession = true;
+
+// Save a session to a database
+async function saveSessionToDB(loss, total, avg, ip) {
+  const data = {
+    ipAddress: ip,
+
+    startTime: sessionStartTime,
+    endTime: new Date(),
+
+    packetLoss: loss,
+    totalPackets: total,
+    averageLatency: avg
+  };
+
+  if (isNewSession) {
+    const response = await axios.post(
+      "http://localhost:3001/api/sessions",
+      data
+    );
+
+    // resolve(response);
+    sessionId[ip] = response["_id"];
+  } else {
+    const response = await axios.put(
+      `http://localhost:3001/api/sessions/${sessionId[ip]}`,
+      data
+    );
+    // resolve(response);
+  }
+}
 
 // Pings the provided IP Address
 const getLatency = function(host) {
@@ -15,25 +52,21 @@ const getLatency = function(host) {
   });
 };
 
-function transformData(data) {
+function prepareData(packets, ip, total) {
   var avg = 0;
   var loss = 0;
   var successes = 0;
 
-  for (let i = 0; i < data.length; i++) {
-    if (data[i] != -1) {
+  for (let i = 0; i < packets.length; i++) {
+    if (packets[i] != -1) {
       successes += 1;
-      avg += (data[i] - avg) / successes;
+      avg += (packets[i] - avg) / successes;
     } else {
       loss += 1;
     }
   }
 
-  for (let i = 0; i < successes.length; i++) {
-    avg += (successes[i] - avg) / i + 1;
-  }
-
-  return [avg, loss, successes];
+  return [loss, total, avg, ip];
 }
 
 // Still gotta think of a description for this one [0]
@@ -45,15 +78,15 @@ async function managePings(hosts) {
   }
 
   for (let i = 0; i < 60; i++) {
-    console.log("i: " + i);
+    // console.log("i: " + i);
 
     for (let j = 0; j < hosts.length; j++) {
       try {
         const response = await getLatency(hosts[j]);
         responses[hosts[j]].push(response);
-        console.log(`${i} - ${hosts[j]}: ${response}`);
+        // console.log(`${i} - ${hosts[j]}: ${response}`);
       } catch (err) {
-        console.log(`${i} - ${hosts[j]}: Falhou`);
+        // console.log(`${i} - ${hosts[j]}: Falhou`);
         responses[hosts[j]].push(-1);
       }
     }
@@ -61,8 +94,15 @@ async function managePings(hosts) {
   }
 
   for (let i = 0; i < hosts.length; i++) {
-    console.log(transformData(responses[hosts[i]]));
+    try {
+      const response = await saveSessionToDB(
+        ...prepareData(responses[hosts[i]], hosts[i], 60)
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
+  isNewSession = false;
 }
 
 // [1]
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const routes = express.Router();
 
 const HostController = require("./controllers/HostController");
+const SessionController = require("./controllers/SessionController");
 
 routes.get("/hosts", HostController.index);
 routes.get("/hosts/:id", HostController.show);
 routes.post("/hosts", HostController.store);
 routes.put("/hosts/:id", HostController.update);
+
+routes.get("/sessions", SessionController.index);
+routes.get("/sessions/:id", SessionController.show);
+routes.post("/sessions", SessionController.store);
+routes.put("/sessions/:id", SessionController.update);
 
 module.exports = routes;
